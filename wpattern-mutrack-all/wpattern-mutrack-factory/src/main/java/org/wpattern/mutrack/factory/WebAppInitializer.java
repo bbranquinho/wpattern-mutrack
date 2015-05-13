319d9d0b17d294d53c352cd665b6d2fb084c72f0
bbranquinho <augustobranquinho@yahoo.com.br>
2015-05-13 03:00:12 -0300

    Moving all configurations from web.xml to a initializer.

---
+++ wpattern-mutrack-all/wpattern-mutrack-factory/src/main/java/org/wpattern/mutrack/factory/WebAppInitializer.java
@@ -0,0 +1,27 @@
+package org.wpattern.mutrack.factory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
+import org.jboss.resteasy.plugins.server.servlet.ResteasyBootstrap;
+import org.jboss.resteasy.plugins.spring.SpringContextLoaderListener;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.request.RequestContextListener;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.addListener(new RequestContextListener());
+		servletContext.addListener(new ResteasyBootstrap());
+		servletContext.addListener(new SpringContextLoaderListener());
+
+		Dynamic dynamic = servletContext.addServlet("Resteasy", new HttpServletDispatcher());
+		dynamic.setLoadOnStartup(1);
+		dynamic.addMapping("/service/*");
+	}
+
+
+}
