18d02af3fe05a13dc23a75bc0ea22fcb9e312fc7
bbranquinho <augustobranquinho@yahoo.com.br>
2015-05-17 02:12:54 -0300

    Use the CORS available on RESTEasy Spring.

--- wpattern-mutrack-all/wpattern-mutrack-factory/src/main/java/org/wpattern/mutrack/factory/utils/WebAppInitializer.java
+++ wpattern-mutrack-all/wpattern-mutrack-factory/src/main/java/org/wpattern/mutrack/factory/utils/WebAppInitializer.java
@@ -3,28 +3,40 @@ package org.wpattern.mutrack.factory.utils;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
 
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
 import org.jboss.resteasy.plugins.server.servlet.ResteasyBootstrap;
 import org.jboss.resteasy.plugins.spring.SpringContextLoaderListener;
+import org.springframework.stereotype.Component;
 import org.springframework.web.WebApplicationInitializer;
 import org.wpattern.mutrack.utils.services.ServiceNames;
 
-import com.thetransactioncompany.cors.CORSFilter;
-
-public class WebAppInitializer implements WebApplicationInitializer {
+@Provider
+@Component
+public class WebAppInitializer extends Application implements WebApplicationInitializer, Feature {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		servletContext.addListener(new ResteasyBootstrap());
 		servletContext.addListener(new SpringContextLoaderListener());
 
-		// TODO augusto.branquinho: use the filter available on RESTEasy Spring.
-		servletContext.addFilter("CORS", new CORSFilter()).addMappingForUrlPatterns(null, false, "/*");
-
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("Resteasy", new HttpServletDispatcher());
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping(ServiceNames.ROOT_PATH + "/*");
+	}
+
+	@Override
+	public boolean configure(FeatureContext context) {
+		CorsFilter corsFilter = new CorsFilter();
+		corsFilter.getAllowedOrigins().add("*");
+		context.register(corsFilter);
+
+		return true;
 	}
 
 }
