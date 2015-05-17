49aeee440dea8966a91385143b583c72e6c8a3a9
Augusto Afonso Borges Branquinho <augustobranquinho@yahoo.com.br>
2015-05-17 02:18:50 -0300

    Update WebAppInitializer.java

--- wpattern-mutrack-all/wpattern-mutrack-factory/src/main/java/org/wpattern/mutrack/factory/utils/WebAppInitializer.java
+++ wpattern-mutrack-all/wpattern-mutrack-factory/src/main/java/org/wpattern/mutrack/factory/utils/WebAppInitializer.java
@@ -3,10 +3,6 @@ package org.wpattern.mutrack.factory.utils;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-<<<<<<< HEAD
-=======
-import javax.ws.rs.core.Application;
->>>>>>> 18d02af3fe05a13dc23a75bc0ea22fcb9e312fc7
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.ext.Provider;
@@ -21,11 +17,7 @@ import org.wpattern.mutrack.utils.services.ServiceNames;
 
 @Provider
 @Component
-<<<<<<< HEAD
 public class WebAppInitializer implements WebApplicationInitializer, Feature {
-=======
-public class WebAppInitializer extends Application implements WebApplicationInitializer, Feature {
->>>>>>> 18d02af3fe05a13dc23a75bc0ea22fcb9e312fc7
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
