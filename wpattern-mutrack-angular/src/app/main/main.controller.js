05a6ec3e3ffce4bc93df0e9b11d301c0e074957e
bbranquinho <augustobranquinho@yahoo.com.br>
2015-06-02 03:14:28 -0300

    Added a modal to save packages.

--- wpattern-mutrack-angular/src/app/main/main.controller.js
+++ wpattern-mutrack-angular/src/app/main/main.controller.js
@@ -1,9 +1,8 @@
 'use strict';
 
-// SERVICES
-
 angular.module('mutrack')
-  .controller('MainCtrl', function ($scope, PackageSrv, SchedulerTrackSrv, localStorageService, ngNotify) {
+  .controller('MainCtrl', function ($scope, $modal, $log, PackageSrv, SchedulerTrackSrv, localStorageService, ngNotify) {
+    // Local storage of public packages.
     var packagesInStore = localStorageService.get('packages');
 
     $scope.packages = packagesInStore || [];
@@ -12,21 +11,53 @@ angular.module('mutrack')
       localStorageService.set('packages', $scope.packages);
     }, true);
 
-    $scope.savePackage = function() {
-      $scope.package.code = $scope.package.code.toUpperCase();
-      $scope.packages.push($scope.package);
-      ngNotify.set('Pacote \'' + $scope.package.code + '\' salvo, buscando o último status!', 'success');
-      PackageSrv.trackLastEvent($scope.package);
-      $scope.package = {};
+    $scope.savePackage = function(pack) {
+      pack.code = pack.code.toUpperCase();
+      $scope.packages.push(pack);
+      ngNotify.set('Pacote \'' + pack.code + '\' salvo, buscando o último status!', 'success');
+      PackageSrv.trackLastEvent(pack);
     };
 
     $scope.deletePackage = function(pack) {
       var indexOf = $scope.packages.indexOf(pack);
-
       $scope.packages.splice(indexOf, 1);
     };
 
+    // Open Modal (Add a New Package)
+    $scope.openAddPackage = function () {
+      var modalInstance = $modal.open({
+        animation: true,
+        templateUrl: 'packageModalContent.html',
+        controller: 'MainModalCtrl',
+        size: 'lg',
+        resolve: {
+          pack: function () {
+          }
+        }
+      });
+
+      modalInstance.result.then(function (pack) {
+        $scope.savePackage(pack);
+      });
+    };
+
+    // Track the last event of all packages.
     PackageSrv.trackMultipleLastEvent(PackageSrv.selectPackagesToTrack($scope.packages));
 
+    // Start a scheduler to track packages.
     SchedulerTrackSrv.track($scope.packages);
+});
+
+// More about modal: https://angular-ui.github.io/bootstrap/
+angular.module('mutrack').controller('MainModalCtrl', function ($scope, $modalInstance, pack) {
+
+  $scope.package = { };
+
+  $scope.save = function () {
+    $modalInstance.close($scope.package);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
